7f9032500fab3bbd86c097f8842e6bbbe9b48568
Kengo Kobayashi <kken91914@gmail.com>
2023-03-04 17:09:48 +0900

    mainImageSection,sentenceSectionを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,35 @@ export default function Home() {
     <>
       <div className={styles.body}>
         <Header />
+        <main className={styles.main}>
+          <section className={styles.mainImageSection}></section>
+          <section className={styles.sentenceSection}>
+            <div className={styles.sentenceBox}>
+              <div className={styles.title}>
+                <h4>コンセプト</h4>
+                <h1>Concept</h1>
+              </div>
+              <div className={styles.sentence}>
+                <p>
+                  React
+                  は、インタラクティブなユーザインターフェイスの作成にともなう苦痛を取り除きます。アプリケーションの各状態に対応するシンプルなViewを設計するだけで、Reactはデータの変更を検知し、関連するコンポーネントだけを効率的に更新、描画します。宣言的なViewを用いてアプリケーションを構築することで、コードはより見通しが立ちやすく、デバッグのしやすいものになります。
+                </p>
+              </div>
+            </div>
+            <div className={styles.sentenceBox}>
+              <div className={styles.title}>
+                <h4>メッセージ</h4>
+                <h1>Message</h1>
+              </div>
+              <div className={styles.sentence}>
+                <p>
+                  React
+                  は、インタラクティブなユーザインターフェイスの作成にともなう苦痛を取り除きます。アプリケーションの各状態に対応するシンプルなViewを設計するだけで、Reactはデータの変更を検知し、関連するコンポーネントだけを効率的に更新、描画します。宣言的なViewを用いてアプリケーションを構築することで、コードはより見通しが立ちやすく、デバッグのしやすいものになります。
+                </p>
+              </div>
+            </div>
+          </section>
+        </main>
       </div>
     </>
   );
