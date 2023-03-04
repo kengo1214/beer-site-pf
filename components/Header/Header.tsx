308b7e6265dd7b35704678eda4d7557e4e939b34
Kengo Kobayashi <kken91914@gmail.com>
2023-03-04 16:37:21 +0900

    Headerコンポーネントを作成・導入

---
+++ components/Header/Header.tsx
@@ -0,0 +1,119 @@
+import Link from "next/link";
+import { useState } from "react";
+import { Link as Scroll } from "react-scroll";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.title}>
+        <Scroll
+          to="/"
+          smooth={true}
+          duration={800}
+          className={styles.link}
+          offset={-100}
+        >
+          <h1>No Beer No Life Tokyo</h1>
+        </Scroll>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <Scroll
+              to="/"
+              smooth={true}
+              duration={800}
+              className={styles.link}
+              offset={-100}
+            >
+              Home
+            </Scroll>
+          </li>
+
+          <li>
+            <Link href="/menu">
+              <div className={styles.link}>Menu</div>
+            </Link>
+          </li>
+
+          <li>
+            <Link href="/blog">
+              <div className={styles.link}>Blog</div>
+            </Link>
+          </li>
+
+          <li>
+            <Scroll
+              to="contact"
+              smooth={true}
+              duration={800}
+              className={styles.link}
+              offset={-100}
+            >
+              Contact
+            </Scroll>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Hamburger Menu */}
+      <div className={styles.hamburgerButton} onClick={handleNav}>
+        {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
+      </div>
+
+      <div className={styles.hamburgerButtonMobile} onClick={handleNav}>
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
+      {/* Hamburger Menu */}
+
+      {/* Menu */}
+      <div className={nav ? styles.menuOpen : styles.menuClose}>
+        <ul>
+          <li onClick={handleNav}>
+            <Scroll
+              to="/"
+              smooth={true}
+              duration={800}
+              offset={-100}
+              onClick={handleNav}
+            >
+              <h1>Home</h1>
+            </Scroll>
+          </li>
+          <li onClick={handleNav}>
+            <Link href="/menu">
+              <h1>Menu</h1>
+            </Link>
+          </li>
+          <li onClick={handleNav}>
+            <Link href="/blog">
+              <h1>Blog</h1>
+            </Link>
+          </li>
+          <li onClick={handleNav}>
+            <Scroll
+              to="contact"
+              smooth={true}
+              duration={800}
+              offset={-100}
+              onClick={handleNav}
+            >
+              <h1>Contact</h1>
+            </Scroll>
+          </li>
+        </ul>
+      </div>
+
+      {/* Menu */}
+    </header>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
+import Header from "../components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div className={styles.body}></div>
+      <div className={styles.body}>
+        <Header />
+      </div>
     </>
   );
 }
