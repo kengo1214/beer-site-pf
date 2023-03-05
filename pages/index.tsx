4decbe4e5e52622a4f01a65f19e53722ca79d846
Kengo Kobayashi <kken91914@gmail.com>
2023-03-05 14:57:55 +0900

    accessSectionを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,6 +80,57 @@ export default function Home() {
               </div>
             </div>
           </section>
+
+          <section className={styles.accessSection}>
+            <div className={styles.accessMain}>
+              <div className={styles.accessTitle}>
+                <h4>アクセス</h4>
+                <h1>Access</h1>
+              </div>
+              <div className={styles.accessBox}>
+                <div className={styles.map}>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3242.2294450324493!2d139.7078890156115!3d35.64671823947073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b4046e3f71d%3A0x85ab1d92ef294edf!2z5oG15q-U5a-_6aeF!5e0!3m2!1sja!2sjp!4v1664439285186!5m2!1sja!2sjp"
+                    className={styles.mapSize}
+                    style={{ border: 0 }}
+                    loading="lazy"
+                  />
+                </div>
+                <div className={styles.sentenceBox}>
+                  <div className={styles.sentenceItem}>
+                    <p className={styles.title}>住所</p>
+                    <p className={styles.sentence}>
+                      〒150-0000 東京都渋谷区恵比寿1-2-3 エビスビル1F
+                    </p>
+                  </div>
+
+                  <div className={styles.sentenceItem}>
+                    <p className={styles.title}>電話番号</p>
+                    <p className={styles.sentence}>000-1234-5678</p>
+                  </div>
+
+                  <div className={styles.sentenceItem}>
+                    <p className={styles.title}>営業時間</p>
+                    <p className={styles.sentence}>
+                      平日 15:00~23:00 / 土日祝13:00~23:00
+                    </p>
+                  </div>
+
+                  <div className={styles.sentenceItem}>
+                    <p className={styles.title}>最寄駅</p>
+                    <div className={styles.outLine}>
+                      <p className={styles.sentence}>
+                        JR山手線・埼京線・湘南新宿ライン「恵比寿駅」徒歩3分
+                      </p>
+                      <p className={styles.sentence}>
+                        東京メトロ日比谷線「恵比寿駅」徒歩3分
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </main>
       </div>
     </>
