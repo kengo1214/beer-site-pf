22f545b2f596d302e72c9c06cbef0c219f45e8ff
Kengo Kobayashi <kken91914@gmail.com>
2023-03-11 17:28:00 +0900

    Menuページを作成

---
+++ pages/menu/all-menu.tsx
@@ -0,0 +1,10 @@
+import styles from "../../styles/all-menu.module.scss";
+import { Link as Scroll } from "react-scroll";
+
+export default function AllMenu() {
+  return (
+    <>
+      <div className={styles.body}></div>
+    </>
+  );
+}
