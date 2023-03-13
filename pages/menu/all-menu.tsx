6d230c15f080632ec9ca4583b602ec122655c265
Kengo Kobayashi <kken91914@gmail.com>
2023-03-13 18:17:12 +0900

    途中経過

--- pages/menu/all-menu.tsx
+++ pages/menu/all-menu.tsx
@@ -1,11 +1,60 @@
 import styles from "../../styles/all-menu.module.scss";
 import Header from "../../components/Header/Header";
+import { Link as Scroll } from "react-scroll";
 
 export default function AllMenu() {
   return (
     <>
-      <Header />
-      <div className={styles.body}></div>
+      <div className={styles.body}>
+        <Header />
+        <nav className={styles.nav}>
+          <div className={styles.insideLine}>
+            <div className={styles.pageTitle}>
+              <h4>全てのメニュー</h4>
+              <h1>All Menu</h1>
+            </div>
+
+            <ul>
+              <li>
+                <Scroll
+                  to="barrel"
+                  smooth={true}
+                  duration={800}
+                  offset={-230}
+                  className={styles.list}
+                >
+                  <h4>樽ビール</h4>
+                  <h1>Barrel Beer</h1>
+                </Scroll>
+              </li>
+              <li>
+                <Scroll
+                  to="barrel"
+                  smooth={true}
+                  duration={800}
+                  offset={-230}
+                  className={styles.list}
+                >
+                  <h4>ボトルビール</h4>
+                  <h1>Bottle Beer</h1>
+                </Scroll>
+              </li>
+              <li>
+                <Scroll
+                  to="barrel"
+                  smooth={true}
+                  duration={800}
+                  offset={-230}
+                  className={styles.list}
+                >
+                  <h4>カクテル</h4>
+                  <h1>Cocktail</h1>
+                </Scroll>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </>
   );
 }
