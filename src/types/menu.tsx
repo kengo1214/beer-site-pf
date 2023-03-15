2e77c4cafdf8422b664b8ac161485450bfc9f254
Kengo Kobayashi <kken91914@gmail.com>
2023-03-15 15:42:34 +0900

    microCMSから取得するデータ型を定義

---
+++ src/types/menu.tsx
@@ -0,0 +1,13 @@
+export type Menu = {
+  // body: string;
+  id: string;
+  title: string;
+  price: string;
+  product: string;
+  detail: string;
+  image: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
