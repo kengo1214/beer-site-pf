82019afca5cd26a4e608fb2a2991577e54166fbc
Kengo Kobayashi <kken91914@gmail.com>
2023-03-15 18:26:03 +0900

    microCMSで設定した画像を使うためにdomainsを設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ["images.microcms-assets.io"],
+  },
+  experimental: {
+    scrollRestoration: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
