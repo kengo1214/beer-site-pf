bfd3154eb47f3fd4fb3ae83602f6b5aaa5892ba7
Kengo Kobayashi <kken91914@gmail.com>
2023-03-16 11:53:49 +0900

    Buttonコンポーネントを作成

---
+++ components/Button/Button.tsx
@@ -0,0 +1,14 @@
+import styles from "./Button.module.scss";
+import Link from "next/link";
+import { AiOutlineDoubleRight } from "react-icons/ai";
+
+export default function Button() {
+  return (
+    <>
+      <Link href="/" className={styles.button}>
+        <p>More</p>
+        <AiOutlineDoubleRight size={18} className={styles.icon} />
+      </Link>
+    </>
+  );
+}
