6bb80b84f5cb7b12af340bd26c7d2ef291119623
Kengo Kobayashi <kken91914@gmail.com>
2023-03-20 17:31:02 +0900

    更新

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,14 +1,15 @@
 import styles from "./Button.module.scss";
-import Link from "next/link";
+// import Link from "next/link";
 import { AiOutlineDoubleRight } from "react-icons/ai";
 
 export default function Button() {
   return (
     <>
-      <Link href="/" className={styles.button}>
+      {/* <Link href="/" className={styles.button}> */}
+      <div className={styles.button}>
         <p>More</p>
         <AiOutlineDoubleRight size={18} className={styles.icon} />
-      </Link>
+      </div>
     </>
   );
 }
