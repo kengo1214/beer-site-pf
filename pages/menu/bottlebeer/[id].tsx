3005db77e0d67f859b09da23a760b62bc804e984
Kengo Kobayashi <kken91914@gmail.com>
2023-03-24 13:08:21 +0900

    pages/menu/bottle,cocktailに動的ページを作成

---
+++ pages/menu/bottlebeer/[id].tsx
@@ -0,0 +1,92 @@
+import styles from "../../../styles/[id].module.scss";
+import { clientMenu } from "../../../libs/client";
+import type { Menu } from "../../../src/types/menu";
+import Image from "next/legacy/image";
+import { useRouter } from "next/router";
+import Header from "../../../components/Header/Header";
+import Footer from "../../../components/Footer/Footer";
+import BuckButton from "../../../components/Button/BackButton";
+
+//getStaticPaths（パスの指定）🔥🔥🔥
+export async function getStaticPaths() {
+  const data = await clientMenu.get({ endpoint: "bottle-beer" });
+
+  const paths = data.contents.map(
+    (content: { id: string }) => `/menu/bottlebeer/${content.id}`
+  );
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+//getStaticProps（情報取得）🔥🔥🔥
+export async function getStaticProps(context: { params: { id: string } }) {
+  const id = context.params.id;
+  const data = await clientMenu.get({ endpoint: "bottle-beer", contentId: id });
+
+  return {
+    props: {
+      bottlebeer: data,
+    },
+  };
+}
+
+//🔥🔥🔥
+type Props = {
+  bottlebeer: Menu;
+};
+
+export default function MoreInformation({ bottlebeer }: Props) {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className={styles.body}>
+        <Header />
+        <section className={styles.section}>
+          <main className={styles.main}>
+            <div className={styles.sectionTitle}>
+              <h4>樽ビール</h4>
+              <h1>Barrel Beer</h1>
+            </div>
+            <div className={styles.itemBox}>
+              <h1 className={styles.title}>{bottlebeer.title}</h1>
+
+              <div className={styles.about}>
+                <p className={styles.price}>{bottlebeer.price}</p>
+                <p
+                  className={styles.product}
+                  dangerouslySetInnerHTML={{
+                    __html: `${bottlebeer.product}`,
+                  }}
+                />
+              </div>
+
+              <p
+                className={styles.detail}
+                dangerouslySetInnerHTML={{
+                  __html: `${bottlebeer.detail}`,
+                }}
+              />
+              <div className={styles.image}>
+                <Image
+                  src={bottlebeer.image.url}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className={styles.back} onClick={() => router.back()}>
+              <BuckButton />
+            </div>
+          </main>
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
+}
---
+++ pages/menu/cocktailhardliquor/[id].tsx
@@ -0,0 +1,95 @@
+import styles from "../../../styles/[id].module.scss";
+import { clientMenu } from "../../../libs/client";
+import type { Menu } from "../../../src/types/menu";
+import Image from "next/legacy/image";
+import { useRouter } from "next/router";
+import Header from "../../../components/Header/Header";
+import Footer from "../../../components/Footer/Footer";
+import BuckButton from "../../../components/Button/BackButton";
+
+//getStaticPaths（パスの指定）🔥🔥🔥
+export async function getStaticPaths() {
+  const data = await clientMenu.get({ endpoint: "cocktail-and-hard-liquor" });
+
+  const paths = data.contents.map(
+    (content: { id: string }) => `/menu/cocktailhardliquor/${content.id}`
+  );
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+//getStaticProps（情報取得）🔥🔥🔥
+export async function getStaticProps(context: { params: { id: string } }) {
+  const id = context.params.id;
+  const data = await clientMenu.get({
+    endpoint: "cocktail-and-hard-liquor",
+    contentId: id,
+  });
+
+  return {
+    props: {
+      cocktail: data,
+    },
+  };
+}
+
+//🔥🔥🔥
+type Props = {
+  cocktail: Menu;
+};
+
+export default function MoreInformation({ cocktail }: Props) {
+  const router = useRouter();
+
+  return (
+    <>
+      <div className={styles.body}>
+        <Header />
+        <section className={styles.section}>
+          <main className={styles.main}>
+            <div className={styles.sectionTitle}>
+              <h4>樽ビール</h4>
+              <h1>Barrel Beer</h1>
+            </div>
+            <div className={styles.itemBox}>
+              <h1 className={styles.title}>{cocktail.title}</h1>
+
+              <div className={styles.about}>
+                <p className={styles.price}>{cocktail.price}</p>
+                <p
+                  className={styles.product}
+                  dangerouslySetInnerHTML={{
+                    __html: `${cocktail.product}`,
+                  }}
+                />
+              </div>
+
+              <p
+                className={styles.detail}
+                dangerouslySetInnerHTML={{
+                  __html: `${cocktail.detail}`,
+                }}
+              />
+              <div className={styles.image}>
+                <Image
+                  src={cocktail.image.url}
+                  layout="fill"
+                  objectFit="contain"
+                  alt="image"
+                />
+              </div>
+            </div>
+
+            <div className={styles.back} onClick={() => router.back()}>
+              <BuckButton />
+            </div>
+          </main>
+        </section>
+        <Footer />
+      </div>
+    </>
+  );
+}
