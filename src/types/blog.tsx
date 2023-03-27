5e6984f8633ff8832576efc1425c3630a435408e
Kengo Kobayashi <kken91914@gmail.com>
2023-03-27 12:08:44 +0900

    microCMSから取得するデータ型を定義

---
+++ src/types/blog.tsx
@@ -0,0 +1,10 @@
+export type Blog = {
+  id: string;
+  title: string;
+  body: string;
+  image: { url: string };
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+};
