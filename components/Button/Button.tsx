a4b13f0683fb6f9988ec93a7a8761d26ca0eadaa
Kengo Kobayashi <kken91914@gmail.com>
2023-04-13 16:04:36 +0900

    import styles from ./HogeButton.module.scssから./Button.module.scssへ修正。関数名もHogeButtonからButtonへ変更

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,11 +1,11 @@
-import styles from "./HogeButton.module.scss";
+import styles from "./Button.module.scss";
 import { AiOutlineRollback } from "react-icons/ai";
 
 interface Props {
   name: string;
 }
 
-export default function HogeButton(props:Props) {
+export default function Button(props:Props) {
   return (
     <>
       <div className={`${styles.button} ${styles.blogButton}`}>
