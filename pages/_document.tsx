76f83a905c626242d3d140e423744564a533ce81
Kengo Kobayashi <kken91914@gmail.com>
2023-04-14 18:04:29 +0900

    GoogleFontsの導入、font-familyの設定。

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.js
+
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cinzel:wght@800&family=Shippori+Mincho+B1:wght@600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
