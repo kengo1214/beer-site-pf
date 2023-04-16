e39ee2c578918733cf870479e54587afc80f2dd9
Kengo Kobayashi <kken91914@gmail.com>
2023-04-16 17:58:14 +0900

    1024px以降Archiveの固定を解除。480px以降のBlog表示を初期状態に揃えた。

--- pages/blog/latest-blog.tsx
+++ pages/blog/latest-blog.tsx
@@ -5,11 +5,10 @@ import { BsFillArrowUpCircleFill } from "react-icons/bs";
 import { BsFillArrowDownCircleFill } from "react-icons/bs";
 import Link from "next/link";
 import Image from "next/legacy/image";
-import dayjs from "dayjs"; 
+import dayjs from "dayjs";
 import { clientBlog } from "../../libs/client";
 import { groupBy } from "../../libs/util";
 import type { Blog } from "../../src/types/blog";
-
 
 export async function getStaticProps() {
   const latestBlogData = await clientBlog.get({
@@ -74,53 +73,40 @@ export default function LatestBlog({ latestBlog, monthlyIndex }: Props) {
             </ul>
           </section>
 
-          <section className={styles.scrollSection}>
-            <div className={styles.scroll}>
-              <Link href="#top">
-                <BsFillArrowUpCircleFill className={styles.scrollTop} />
-              </Link>
-              <Link href="#down">
-                <BsFillArrowDownCircleFill className={styles.scrollDown} />
-              </Link>
-            </div>
-          </section>
+          <section className={styles.blogSection} id="top">
+            {latestBlog.map((latestBlog) => (
+              <Link
+                href={`/blog/${latestBlog.id}`}
+                className={styles.link}
+                key={latestBlog.id}
+              >
+                <div className={styles.blog}>
+                  <div className={styles.articleBox}>
+                    <p className={styles.title}>{latestBlog.title}</p>
+                    <p className={styles.publishedAt}>
+                      {dayjs(latestBlog.publishedAt).format(
+                        "YYYY年MM月DD日 HH:mm"
+                      )}
+                    </p>
+                  </div>
 
-          <section className={styles.outlineSection}>
-            <section className={styles.blogSection} id="top">
-              {latestBlog.map((latestBlog) => (
-                <Link
-                  href={`/blog/${latestBlog.id}`}
-                  className={styles.link}
-                  key={latestBlog.id}
-                >
-                  <div className={styles.blog}>
-                    <div className={styles.articleBox}>
-                      <p className={styles.title}>{latestBlog.title}</p>
-                      <p className={styles.publishedAt}>
-                                        
-                        {dayjs(latestBlog.publishedAt).format("YYYY年MM月DD日 HH:mm")}
-                        
-                      </p>
-                    </div>
-
-                    <div className={styles.imageBox}>
-                      <div className={styles.image}>
-                        <Image
-                          src={latestBlog.image.url}
-                          layout="fill"
-                          objectFit="cover"
-                          alt="image"
-                        />
-                      </div>
+                  <div className={styles.imageBox}>
+                    <div className={styles.image}>
+                      <Image
+                        src={latestBlog.image.url}
+                        layout="fill"
+                        objectFit="cover"
+                        alt="image"
+                      />
                     </div>
                   </div>
-                </Link>
-              ))}
-            </section>
-            <footer className={styles.footer} id="down">
-              <Footer />
-            </footer>
+                </div>
+              </Link>
+            ))}
           </section>
+          <footer className={styles.footer} id="down">
+            <Footer />
+          </footer>
         </section>
       </div>
     </>
