06ac1628758f135dccb83836379f7927ac402ab7
Kengo Kobayashi <kken91914@gmail.com>
2023-04-20 11:14:53 +0900

    <Html lang=ja>を設定

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="ja">
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Cinzel:wght@800&family=Shippori+Mincho+B1:wght@600;700&display=swap"
