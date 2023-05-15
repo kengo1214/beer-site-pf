8b87ae3311a7dc39e0cd66599df39a347e701c05
Kengo Kobayashi <kken91914@gmail.com>
2023-05-15 12:58:26 +0900

    next/headの設定を追加した

--- pages/contact-done.tsx
+++ pages/contact-done.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 import styles from "../styles/contact-done.module.scss";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
@@ -7,6 +8,18 @@ import Button from "../components/Button/Button";
 export default function ContactDone() {
   return (
     <>
+      <Head>
+        <title>送信完了 | No Beer No Life Tokyo</title>
+        <meta
+          name="description"
+          content="お問い合わせいただきまして、誠にありがとうございます。
+          追って担当者よりご連絡を差し上げますので、今しばらくお待ちくださいませ。"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/image/beer-favicon.png" />
+      </Head>
+
       <div className={styles.body}>
         <Header />
         <main className={styles.main}>
